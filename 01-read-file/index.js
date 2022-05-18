c959b47599f81a6b9900fde8db5b5be97756cbc5
VoitehovichXenia <voitehovichkg@gmail.com>
2022-05-18 19:27:33 +0300

    feat: add solution to 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readFile(
+  path.join(__dirname, 'text.txt'),
+  'utf-8',
+  (error, data) => {
+    if (error) throw error;
+    console.log(data);
+  }
+);
