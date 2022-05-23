b9ad6cd80b156c5050f948f16f9746f77fdf2b13
VoitehovichXenia <voitehovichkg@gmail.com>
2022-05-23 22:18:11 +0300

    refactor: add async/await for beetter readability at 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,27 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readdir(
-  path.join(__dirname, 'styles'),
-  (err, files) => {
-    if (err) throw err;
-    for (let i = 0; i < files.length; i++) {
-      const extension = path.extname(files[i]);
-      if (extension === '.css') {
-        fs.readFile(
-          path.join(__dirname, 'styles', files[i]),
-          'utf-8',
-          (err, data) => {
-            if (err) throw err;
-            writeToBundle(data);
-          }
-        );
-      }      
-    }
-  }
-);
+(async function bundleCSS() {
+  const files = await fs.promises.readdir(
+    path.join(__dirname, 'styles')
+  )
 
-function writeToBundle(file) {
   fs.open(
     path.join(__dirname, 'project-dist', 'bundle.css'),
     'w',
@@ -30,11 +14,26 @@ function writeToBundle(file) {
     }
   );
 
-  fs.appendFile(
-    path.join(__dirname, 'project-dist', 'bundle.css'),
-    file,
-    err => {
-      if (err) throw err;
-    }
-  );  
-}
+  for (let i = 0; i < files.length; i++) {
+    const extension = path.extname(files[i]);
+    if (extension === '.css') {
+      fs.readFile(
+        path.join(__dirname, 'styles', files[i]),
+        'utf-8',
+        (err, data) => {
+          if (err) throw err;
+          fs.appendFile(
+            path.join(__dirname, 'project-dist', 'bundle.css'),
+            data,
+            err => {
+              if (err) throw err;
+            }
+          );  
+        }
+      );
+    }      
+  }
+
+  return console.log('Styles was bundled');
+})();
+
