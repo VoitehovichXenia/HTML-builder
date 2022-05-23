33feac37ece96c648f016ffdcaf5799f2063499b
VoitehovichXenia <voitehovichkg@gmail.com>
2022-05-23 21:22:25 +0300

    fix: change converting to kb formula at 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,25 +1,20 @@
 const fs = require('fs');
 const path = require('path');
-const secretFolder = path.join(__dirname, 'secret-folder');
+const secretFolderPath = path.join(__dirname, 'secret-folder');
 
+(async function showFiles() {
+  const items = await fs.promises.readdir(secretFolderPath);
 
-fs.readdir(
-  path.join(__dirname, 'secret-folder'),
-  (err, items) => {
-    if (err) throw err;
-    for (let i = 0; i < items.length; i++) {
-      fs.stat(
-        path.join(secretFolder, items[i]), 
-        (err, stats) => {
-          if (err) throw err;
-          if (!stats.isDirectory()) {
-            const dotIndex = items[i].indexOf('.');
-            const name = items[i].slice(0, dotIndex);
-            const extension = items[i].slice(dotIndex + 1);
-            console.log(`${name} - ${extension} - ${stats.size / 1000}kb`);
-          }
-        }
-      );
+  for (let i = 0; i < items.length; i++) {
+    const fileStats = await fs.promises.stat(
+      path.join(secretFolderPath, items[i])
+    );
+
+    if (!fileStats.isDirectory()) {
+      const dotIndex = items[i].indexOf('.');
+      const name = items[i].slice(0, dotIndex);
+      const extension = items[i].slice(dotIndex + 1);
+      console.log(`${name} - ${extension} - ${(fileStats.size / 1024).toFixed(3)}kb`);
     }
   }
-);
+})();
